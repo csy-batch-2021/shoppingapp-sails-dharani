7c236cb47c29f715a7be47ebebca0ba9b902480c
Dharaneeshwaran97 <58076976+Dharaneeshwaran97@users.noreply.github.com>
2021-03-01 14:59:21 +0530

    format changes

--- api/validator/productValidator.js
+++ api/validator/productValidator.js
@@ -21,14 +21,12 @@ class ProductValidator {
     static validateNewProduct(product) {
         if (product.name === null || product.name.trim().length === 0) {
             throw new Error('please enter validate Product Name');
-        }
-        else if (
+        } else if (
             product.brandName === null ||
             product.brandName.trim().length === 0
         ) {
             throw new Error('please enter validate BrandName ');
-        }
-        else if (product.ram === null || product.ram <= 0) {
+        } else if (product.ram === null || product.ram <= 0) {
             throw new Error('please enter validate Ram');
         } else if (product.price === null || product.price <= 0) {
             throw new Error('please enter validate Price');
